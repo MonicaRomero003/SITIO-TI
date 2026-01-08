fb1df1f8f2922cb8b50ead5fc4bbb4e906de5e14
Monica Itzel Romero Calva <240498@utxicotepec.edu.mx>
2026-01-08 14:29:26 -0600

    update: se agregaron funciones JS y estilos en CSS

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,31 @@
 function saludar(){
     alert("olA cara de b0l4");
 }
+
+function suma(){
+    var valor1= 12;
+    var valor2= 11;
+    var resultado= valor1+valor2;
+    alert("El resultado de la suma de "+valor1+"+"+valor2+" es: "+resultado)
+}
+
+function resta(){
+    var valor1= 12;
+    var valor2= 11;
+    var resultado= valor1-valor2;
+    alert("El resultado de la resta de "+valor1+"-"+valor2+" es: "+resultado)
+}
+
+function producto(){
+    var valor1= 5;
+    var valor2= 5;
+    var resultado= valor1*valor2;
+    alert("El resultado del producto de "+valor1+"*"+valor2+" es: "+resultado)
+}
+
+function dividir(){
+    var valor1= 10;
+    var valor2= 5;
+    var resultado= valor1/valor2;
+    alert("El resultado de divir "+valor1+"/"+valor2+" es: "+resultado)
+}
