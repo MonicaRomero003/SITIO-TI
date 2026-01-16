470693a5eef3a9a98f48d4e357d7551f23d83802
Monica Itzel Romero Calva <240498@utxicotepec.edu.mx>
2026-01-16 14:13:54 -0600

    add: se agregaron funciones en JS y los botones que las hacen funcionar

--- JS/script.js
+++ JS/script.js
@@ -29,3 +29,30 @@ function dividir(){
     var resultado= valor1/valor2;
     alert("El resultado de divir "+valor1+"/"+valor2+" es: "+resultado)
 }
+
+/*funciones*/
+function numero(){
+    let num1=10; 
+    let num2=2;
+    num1 = num1 + 1; num2 = num2 + 5;
+    alert ("El primer numero ahora es "+num1); 
+    alert ("El segundo numero ahora es "+num2 );
+}
+function concatenar(){
+    const a = "Hola";
+    let	b = " mundo!";
+
+    alert("Constante a contiene " + a ); 
+    alert("Variable b contiene " + b ); 
+    alert(a + b );
+}
+function ptsdeinterrupcion(){
+    let a, b;
+    a = 2; b = 8;
+    resultado = a * b;
+
+    document.write(`Variable a contiene ${a} <br> ` );
+    document.write(`Variable b contiene ${b} <br> ` ); 
+    document.write(`El producto de a por b es ${resultado} `);
+
+}
